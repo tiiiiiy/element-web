e27721e71b18b05388dcf282b9215510f20c20d4
tiiiiiy <97773341+tiiiiiy@users.noreply.github.com>
2024-12-05 20:35:01 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -x
 
-# configure here
-ELEMENT_VERSION=v1.11.87
-# end configure here
+ELEMENT_VERSION=$(curl -s https://api.github.com/repos/vector-im/element-web/releases/latest | jq -r '.tag_name')
+
+if [ -z "$ELEMENT_VERSION" ]; then
+  echo "Failed to get the latest version"
+  exit 1
+fi
 
 curl -L -o element.tar.gz https://github.com/vector-im/element-web/releases/download/$ELEMENT_VERSION/element-$ELEMENT_VERSION.tar.gz
 
