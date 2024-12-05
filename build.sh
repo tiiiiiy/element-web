b4f2ef48342b56ca99d36c1efb6864a3ffe40542
tiiiiiy <97773341+tiiiiiy@users.noreply.github.com>
2024-12-05 20:39:07 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-ELEMENT_VERSION=$(curl -s https://api.github.com/repos/vector-im/element-web/releases/latest | jq -r '.tag_name')
+ELEMENT_VERSION=$(curl -s https://api.github.com/repos/vector-im/element-web/releases/latest | grep -oP '"tag_name": "\K(.*?)(?=")')
 
 if [ -z "$ELEMENT_VERSION" ]; then
   echo "Failed to get the latest version"
